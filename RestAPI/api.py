9bfef1ee7acce280b7512b470da7c67085fac3ab
Peter Sulcs <Peter.Sulcs@gatech.edu>
2016-09-27 21:00:00 -0400

    refactored to have one Sensor endpoint with get and put

--- RestAPI/api.py
+++ RestAPI/api.py
@@ -5,30 +5,8 @@ from flask_restful import reqparse
 app = Flask(__name__)
 api = Api(app)
 
-class PostMeasurement(Resource):
-    def post(self):
-        try:
-            # parse the arguments
-            parser = reqparse.RequestParser()
+class Sensor(Resource):
 
-            parser.add_argument('name', type=str, 
-                help='Unique name of sensor', required=True)
-
-            parser.add_argument('value', type=float, 
-                help='The measurement value', required=True)
-
-            parser.add_argument('timestamp', type=str, 
-                help='The time and date of the measurement in format "YYYY-MM-DD HH:MM:SS"', required=True)
-            args = parser.parse_args()
-            _measurementName = args['name']
-            _measurementValue = args['value']
-            _measurementTimestamp = args['timestamp']
-        except Exception as e:
-            return {'error': str(e)}
-
-        return {'name': args['name'], 'value': args['value'], 'timestamp': args['timestamp']}
-
-class GetSensor(Resource):
     def get(self, sensor_name):
         # try:
         #     parser = reqparse.RequestParser()
@@ -43,8 +21,28 @@ class GetSensor(Resource):
 
         return {'name': sensor_name}
 
-api.add_resource(PostMeasurement, '/PostMeasurement')
-api.add_resource(GetSensor, '/GetSensor/<string:sensor_name>')
+    def put(self, sensor_name):
+        try:
+            # parse the arguments
+            parser = reqparse.RequestParser()
+
+            # parser.add_argument('name', type=str, 
+            #     help='Unique name of sensor', required=True)
+
+            parser.add_argument('value', type=float, 
+                help='The measurement value', required=True)
+
+            parser.add_argument('timestamp', type=str, 
+                help='The time and date of the measurement in format "YYYY-MM-DD HH:MM:SS"', required=True)
+            args = parser.parse_args()
+            _measurementValue = args['value']
+            _measurementTimestamp = args['timestamp']
+        except Exception as e:
+            return {'error': str(e)}
+
+        return {'name': sensor_name, 'value': args['value'], 'timestamp': args['timestamp']}
+
+api.add_resource(Sensor, '/Sensor/<string:sensor_name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
